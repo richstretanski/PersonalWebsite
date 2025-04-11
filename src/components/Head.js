916484f129e3a78b4aafe7aced98133905a625ee
richstretanski <rstretan@umich.edu>
2025-04-11 14:12:20 -0400

    mobile styling, favicon, 404

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { HelmetProvider, Helmet } from 'react-helmet-async'; // Import from react-helmet-async
-import favicon from '../images/gamertag.png'; // Import the favicon
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import '../styles/style.css'; // Import the global style
 
 const Head = () => {
@@ -9,11 +8,8 @@ const Head = () => {
       <Helmet>
         <meta charSet="UTF-8" />
         <title>Rich Stretanski</title>
-        
-        <link rel="stylesheet" href="/styles/style.css" /> {/* Add styles/style.css */}
-        <link rel="icon" type="image/x-icon" href={favicon} /> {/* Use imported favicon */}
+        <link rel="stylesheet" href="/styles/style.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* Ensure the script tag has valid attributes */}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/draggabilly/2.2.0/draggabilly.pkgd.min.js" type="text/javascript"></script>
       </Helmet>
     </HelmetProvider>
