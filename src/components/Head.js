0e4fa998c04e76af94739f1a42b055f895f6cb63
richstretanski <rstretan@umich.edu>
2025-04-11 13:24:30 -0400

    removed stylesheets from head

--- src/components/Head.js
+++ src/components/Head.js
@@ -9,8 +9,7 @@ const Head = () => {
       <Helmet>
         <meta charSet="UTF-8" />
         <title>Rich Stretanski</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" />
-        <link rel="stylesheet" href="https://unpkg.com/98.css" />
+        
         <link rel="stylesheet" href="/styles/style.css" /> {/* Add styles/style.css */}
         <link rel="icon" type="image/x-icon" href={favicon} /> {/* Use imported favicon */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
